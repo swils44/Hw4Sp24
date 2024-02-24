f1dbcf42e8ce59feaf834f0c1cbfa634230ef719
Austin Biggerstaff <67385046+Biggerstaff22@users.noreply.github.com>
2024-02-24 15:32:03 -0600

    Problem B is now finished.

--- Hw4Sp24B.py
+++ Hw4Sp24B.py
@@ -1,13 +1,10 @@
-# Problem finished by Austin.
+# Problem finished by Austin
 import numpy as np
 from scipy.optimize import fsolve
 
-#I collaborated with ChatGPT to create this code. This is the code that Chat gave me. I told it that it could use
-#numpy and scipy to write the code.
-
 def func1(x):
     """
-    Here, we are defining our first function: x - 3cos(x)
+    Function definition for the first function: x - 3cos(x)
 
     Parameters:
     x (float or array_like): Input value(s) for which the function value is calculated.
@@ -15,11 +12,11 @@ def func1(x):
     Returns:
     float or array_like: Value(s) of the function at the input value(s) x.
     """
-    return x - 3 * np.cos(x)  # Using np.cos to handle array-like input
+    return x - 3 * np.cos(x)
 
 def func2(x):
     """
-    Here, we are defining our first function: cos(2x) * x^3
+    Function definition for the second function: cos(2x) * x^3
 
     Parameters:
     x (float or array_like): Input value(s) for which the function value is calculated.
@@ -35,8 +32,17 @@ roots_func1 = fsolve(func1, [-5, 5])
 # Use fsolve to find the roots of the second function
 roots_func2 = fsolve(func2, [-5, 5])
 
+# Check if there are any intersections
+intersection_points = [root for root in roots_func1 if root in roots_func2]
+
 # Print the roots of the first function
 print("Roots of x - 3cos(x) = 0:", roots_func1)
 
 # Print the roots of the second function
 print("Roots of cos(2x) * x^3 = 0:", roots_func2)
+
+# Print the intersection points if any
+if intersection_points:
+    print("The functions intersect at the following point(s):", intersection_points)
+else:
+    print("The functions do not intersect within the specified range.")
